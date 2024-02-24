639665c589346bad9565c9737555ffb7a3564b2b
velderyx <adamvelder@gmail.com>
2024-02-24 11:46:43 +0700

    mid login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login.index', [
+            'title' => 'Login',
+            'active' => 'login'
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/part');
 });
+
+Route::get('/login',[LoginController::class, 'index'])->name('login');
+Route::post('/login',[LoginController::class, 'authenticate'])->name('login');
+
 
 Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('layout.main');
 
