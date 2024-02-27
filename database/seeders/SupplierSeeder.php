6ca09cf023dfd39ab4f780db05091cc2cbf3e47c
velderyx <adamvelder@gmail.com>
2024-02-27 16:35:03 +0700

    suppliers seed

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -14,13 +14,34 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
-        $supplierNames = [
-            "Nicho Jaya",
-            "Nusantara",
-            "Surya Elang Mas"
+
+
+        $suppliersNames = [
+            "lainnya",
+            "Sarinah Jaya",
+            "Nusantara / Oto sentosa",
+            "Wira Utama",
+            "Tunas Motor",
+            "Delta Terus Jaya",
+            "Trijaya Motor",
+            "SJM",
+            "Rajawali",
+            "Garuda Totalindo",
+            "Putra Jaya",
+            "Champion",
+            "Surya Mas",
+            "Cahaya Putra",
+            "Auto Prima",
+            "Delta Terus Jaya",
+            "Putra Jaya",
+            "Tri Sakti",
+            "Step Auto",
+            "Karya Abadi",
+            "Prioritas",
+            "Buana Sakti"
         ];
-        
-        foreach ($supplierNames as $name) {
+
+        foreach ($suppliersNames as $name) {
             DB::table('suppliers')->insert([
                 'name' => $name,
             ]);
