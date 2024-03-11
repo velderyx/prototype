7ed116857b87104a3d56c6520cd7b7dce9c73cbf
velderyx <adamvelder@gmail.com>
2024-03-11 17:43:08 +0700

    mid barang

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -33,7 +33,7 @@
 
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Striped Full Width Table</h3>
+                <h3 class="card-title">Compound 2500</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body p-0">
@@ -41,51 +41,26 @@
                   <thead>
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
+                      <th>Tgl</th>
+                      <th>Entitas</th>
+                      <th style="width: 10px">Jumlah</th>
+                      <th style="width: 10px">Saldo</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td>1.</td>
-                      <td>Update software</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-danger">55%</span></td>
+                      <td>05/07/2024</td>
+                      <td>Andreas</td>
+                      <td>1</td>
+                      <td>20</td>
                     </tr>
                     <tr>
                       <td>2.</td>
-                      <td>Clean database</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-warning">70%</span></td>
-                    </tr>
-                    <tr>
-                      <td>3.</td>
-                      <td>Cron job running</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-primary">30%</span></td>
-                    </tr>
-                    <tr>
-                      <td>4.</td>
-                      <td>Fix and squish bugs</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
+                      <td>02/03/2024</td>
+                      <td>yadi</td>
+                      <td>-5</td>
+                      <td>20</td>
                     </tr>
                   </tbody>
                 </table>
