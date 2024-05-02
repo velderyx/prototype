46cb5cda470bc6fd754cf3a894f581f0ba166534
velderyx <adamvelder@gmail.com>
2024-05-02 14:09:37 +0700

    minor

--- resources/views/parts/index2.blade.php
+++ resources/views/parts/index2.blade.php
@@ -5,7 +5,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Data Part Keluar
+            <h1>Data Part Archive
             </h1>
           </div>
           <div class="col-sm-6">
