d02c6e3462ad56527d2e72239d20cceeec19d8b9
velderyx <adamvelder@gmail.com>
2024-05-20 14:56:53 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,7 @@ route::post('/test', [TestController::class, 'store'])->name('test.store');
 
 
 
+//auth using middleware
 // Route::get('/login',[LoginController::class, 'index'])->name('login')->middleware('guest');
 // Route::post('/login',[LoginController::class, 'authenticate']);
 // Route::post('/logout',[LoginController::class, 'logout']);
